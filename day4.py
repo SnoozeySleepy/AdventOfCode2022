40cf08656aebb6c86bf0d14f401e7e5ac0bcd7db
Christian <rednoise007@gmail.com>
2022-12-04 10:31:40 -0800

    Add day 4

---
+++ day4.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+sections = [ [list(map(int, s.split('-'))) for s in line.split(',')] for line in open("day4_input").read().splitlines()]
+
+full_overlap = sum([a >= c and b <= d or c >= a and d <= b for (a,b),(c,d) in sections])
+print(full_overlap)
+
+overlap = sum([a <= c <= b or a <= d <= b or c <= a <= d or c <= b <= d for (a,b),(c,d) in sections])
+print(overlap)
